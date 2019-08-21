44593acb33f69134f1ab27ea7b712acc1a67015a
Shah <armanshah360@gmail.com>
2019-08-21 15:12:49 -0700

    added ProtectedRoute

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -44,7 +44,7 @@ export default function(state = initialState, action) {
         ...state,
         token: null,
         isAuthenticated: false,
-        loading: false
+        loading: true
       };
     default:
       return state;
